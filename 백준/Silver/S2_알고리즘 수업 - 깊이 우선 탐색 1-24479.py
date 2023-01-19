30a91b7eccb531414da5fe703987281e25a689b1
Min <esspacetoM@gmail.com>
2023-01-19 23:21:08 +0900

    Create S2_알고리즘 수업 - 깊이 우선 탐색 1-24479.py

---
+++ 백준/Silver/S2_알고리즘 수업 - 깊이 우선 탐색 1-24479.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
+n, m, start = map(int, sys.stdin.readline().rstrip().split())
+a = [[] for _ in range(n + 1)]
+check = [0] * (n + 1)
+cnt = 1
+
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().rstrip().split())
+    a[u].append(v)
+    a[v].append(u)
+    
+def dfs(x):
+    global cnt
+    check[x] = cnt
+    a[x].sort()
+    
+    for y in a[x]:
+        if check[y] == 0:
+            cnt += 1
+            dfs(y)
+            
+dfs(start)
+
+for i in range(1, n + 1):
+    print(check[i])
