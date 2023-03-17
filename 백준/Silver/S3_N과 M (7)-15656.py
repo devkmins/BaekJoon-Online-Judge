50a002e1b05bf1ecfbaafa6b760dcd33e3eb3834
Min <esspacetoM@gmail.com>
2023-03-17 17:17:07 +0900

    Update S3_N과 M (7)-15656.py

--- 백준/Silver/S3_N과 M (7)-15656.py
+++ 백준/Silver/S3_N과 M (7)-15656.py
@@ -1,17 +1,17 @@
 import sys
 
-def go(index, n, m, nums_list):
+def go(index, n, m):
     if index == m:
         sys.stdout.write(" ".join(map(str, answer)) + "\n")
         return
     
     for i in range(n):
         answer[index] = nums_list[i]
-        go(index + 1, n, m, nums_list)
+        go(index + 1, n, m)
 
 n, m = map(int, input().split())
 nums_list = list(map(int, input().split()))
 nums_list.sort()
 answer = [0] * m
 
-go(0, n, m, nums_list)
+go(0, n, m)
