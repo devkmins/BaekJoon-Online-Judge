c2826f284e58d24be24a646a99fd9a5746871493
Min <esspacetoM@gmail.com>
2023-03-21 18:28:44 +0900

    Create S2_N과 M (12)-15666.py

---
+++ 백준/Silver/S2_N과 M (12)-15666.py
@@ -0,0 +1,21 @@
+def go(index, start, n, m):
+    if index == m:
+        d.append(tuple(answer))
+        return
+
+    for i in range(start, n):
+        answer[index] = nums_list[i]
+        go(index + 1, i, n, m)
+
+n, m = map(int, input().split())
+nums_list = list(map(int, input().split()))
+nums_list.sort()
+answer = [0] * m
+d = []
+
+go(0, 0, n, m)
+d = list(set(d))
+d.sort()
+
+for i in d:
+    print(" ".join(map(str, i)))
